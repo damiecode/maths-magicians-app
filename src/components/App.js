6ed484bff9e17c827334d6c2ca81313b206e7a3c
damiecode <codenlyn@gmail.com>
2020-04-29 17:19:26 +0100

    Correct path to App in index page

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div id="App">
+      <div className="App">
         <Display />
         <ButtonPanel />
       </div>
