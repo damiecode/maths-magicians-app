bfe76723d76fa5315db2b4aa1ab5efd70b5da668
damiecode <codenlyn@gmail.com>
2020-05-01 09:24:20 +0100

    Add ccalculations and logic to the calculator

---
+++ src/logic/calculate.js
@@ -0,0 +1,43 @@
+import operate from './operate';
+
+const calculate = (props, buttonName) => {
+  let { total, next, operation } = props;
+  const operations = ['+', 'x', '-', '÷'];
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+  if (buttonName === 'AC') {
+    [total, next, operation] = [null, null, null];
+  }
+  if (buttonName === '+/-') {
+    if (total) (total *= -1);
+    if (next) (next *= -1);
+    operation = null;
+    [total, next, operation] = [total *= -1, next *= -1, null];
+  }
+  if (buttonName === '%') {
+    if (total) next = 0.01 * total;
+  }
+
+  if (buttonName === '=') {
+    if (total && next && operation) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+
+    if (operations.includes(buttonName)) {
+      operation = buttonName;
+    } else if (operation && numbers.includes(buttonName)) {
+      next = next ? next + buttonName : buttonName;
+    } else if (numbers.includes(buttonName)) {
+      total = total ? total + buttonName : buttonName;
+    }
+  }
+  return {
+    operation,
+    next,
+    total,
+  };
+};
+
+export default calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,23 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operations) => {
+  const [one, two] = [Big(numberOne), Big(numberTwo)];
+
+  switch (operations) {
+    case '+':
+      return one.plus(two).toString();
+    case '-':
+      return one.minus(two).toString();
+    case '*':
+      return one.times(two).toString();
+    case '÷':
+      if (two.toString() === '0') {
+        return 0;
+      }
+      return one.div(two).toString();
+    default:
+      return '0';
+  }
+};
+
+export default operate;
