872fa87a6253b1d22d99dd6f843d7397a05cdbf9
damiecode <codenlyn@gmail.com>
2020-05-04 09:19:43 +0100

    Initialize handle click and pass to button panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Display from './Display';
@@ -8,14 +9,24 @@ import '../styles/styles.css';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    this.setState(calculate(this.state, buttonName));
   }
 
   render() {
+    const { next, total, operation } = this.state;
     return (
       <div className="app">
-        <Display />
-        <ButtonPanel />
+        <Display operation={operation} next={next || '0'} total={total || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
